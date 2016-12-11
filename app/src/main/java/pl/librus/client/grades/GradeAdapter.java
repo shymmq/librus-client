13b5ecbd69a65455b00d35ee5711afa599882b02
Adam Żbikowski <thehdplayerpl@gmail.com>
2016-12-11 15:02:48 +0100

    Added basic grade details popup

--- app/src/main/java/pl/librus/client/grades/GradeAdapter.java
+++ app/src/main/java/pl/librus/client/grades/GradeAdapter.java
@@ -3,23 +3,35 @@ package pl.librus.client.grades;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.bignerdranch.expandablerecyclerview.ExpandableRecyclerAdapter;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import pl.librus.client.R;
 import pl.librus.client.api.Grade;
 import pl.librus.client.api.GradeCategory;
+import pl.librus.client.api.Lesson;
 import pl.librus.client.api.LibrusData;
 import pl.librus.client.api.Subject;
+import pl.librus.client.api.Teacher;
 
 /**
  * Created by szyme on 09.12.2016. librus-client
@@ -31,6 +43,8 @@ class GradeAdapter extends ExpandableRecyclerAdapter<GradesFragment.GradeListCat
     private final List<GradesFragment.GradeListCategory> categories;
     private LayoutInflater inflater;
     private Map<String, GradeCategory> gradeMap;
+    private Map<String, Subject> subjectMap;
+    private Map<String, Teacher> teacherMap;
 
     /**
      * Primary constructor. Sets up {@link #mParentList} and {@link #mFlatItemList}.
@@ -49,10 +63,12 @@ class GradeAdapter extends ExpandableRecyclerAdapter<GradesFragment.GradeListCat
      *                   adapter is linked to
      * @param context
      */
-    GradeAdapter(@NonNull List<GradesFragment.GradeListCategory> parentList, Map<String, GradeCategory> gradeMap, Context context) {
+    GradeAdapter(@NonNull List<GradesFragment.GradeListCategory> parentList, Map<String, GradeCategory> gradeMap, Map<String, Subject> subjectMap, Map<String, Teacher> teacherMap, Context context) {
         super(parentList);
         this.categories = parentList;
         this.gradeMap = gradeMap;
+        this.subjectMap = subjectMap;
+        this.teacherMap = teacherMap;
         inflater = LayoutInflater.from(context);
     }
 
@@ -73,7 +89,7 @@ class GradeAdapter extends ExpandableRecyclerAdapter<GradesFragment.GradeListCat
             categories.add(new GradesFragment.GradeListCategory(entry.getValue(), subjectMap.get(entry.getKey()).getName()));
         }
         Collections.sort(categories);
-        return new GradeAdapter(categories, data.getGradeCategoriesMap(), data.getContext());
+        return new GradeAdapter(categories, data.getGradeCategoriesMap(), data.getSubjectMap(), data.getTeacherMap(), data.getContext());
     }
 
     @NonNull
@@ -97,7 +113,36 @@ class GradeAdapter extends ExpandableRecyclerAdapter<GradesFragment.GradeListCat
     }
 
     @Override
-    public void onBindChildViewHolder(@NonNull GradeViewHolder childViewHolder, int parentPosition, int childPosition, @NonNull Grade child) {
+    public void onBindChildViewHolder(@NonNull GradeViewHolder childViewHolder, int parentPosition, int childPosition, @NonNull final Grade child) {
         childViewHolder.bind(child, gradeMap.get(child.getCategoryId()));
+        RelativeLayout background = (RelativeLayout) childViewHolder.itemView.getRootView();
+        final Context context = background.getContext();
+        background.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: handle retakes, show semester number
+                MaterialDialog.Builder builder = new MaterialDialog.Builder(context).title("Szczegóły oceny").positiveText("Zamknij");
+                LayoutInflater inflater = LayoutInflater.from(context);
+                View details = inflater.inflate(R.layout.grade_details, null);
+
+                TextView gradeView = (TextView) details.findViewById(R.id.grade_detials_grade);
+                TextView weightView = (TextView) details.findViewById(R.id.grade_details_weight);
+                TextView categoryView = (TextView) details.findViewById(R.id.grade_details_category);
+                TextView subjectView = (TextView) details.findViewById(R.id.grade_details_subject);
+                TextView dateView = (TextView) details.findViewById(R.id.grade_details_date);
+                TextView addDateView = (TextView) details.findViewById(R.id.grade_details_addDate);
+                TextView addedByView = (TextView) details.findViewById(R.id.grade_details_addedBy);
+
+                gradeView.setText(child.getGrade());
+                weightView.setText(String.valueOf(gradeMap.get(child.getCategoryId()).getWeight()));
+                categoryView.setText(gradeMap.get(child.getCategoryId()).getName());
+                subjectView.setText(subjectMap.get(child.getSubjectId()).getName());
+                dateView.setText(child.getDate().toString("EEEE, d MMMM yyyy", new Locale("pl")));
+                addDateView.setText(child.getAddDate().toString("EEEE, HH:mm, d MMMM yyyy", new Locale("pl")));
+                addedByView.setText(teacherMap.get(child.getAddedById()).getName());
+
+                builder.customView(details, true).show();
+            }
+        });
     }
 }
