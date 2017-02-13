83bdbfe9593d3971a07836b8808527dce0fd4252
Szymek <szymek.wysocki@gmail.com>
2017-02-13 17:19:41 +0100

    Fixed TimetableUtilTest

--- app/src/test/java/pl/librus/client/TimetableUtilTest.java
+++ app/src/test/java/pl/librus/client/TimetableUtilTest.java
@@ -16,39 +16,7 @@ import pl.librus.client.timetable.TimetableUtils;
 
 public class TimetableUtilTest {
     @Test
-    public void shouldReturnMondayOnMonday() {
-        //given
-        LocalDate today = new LocalDate(2016, 12, 19);
-        //when
-        List<LocalDate> result = TimetableUtils.getNextFullWeekStarts(today);
-        //then
-        Assert.assertEquals(Lists.newArrayList(today), result);
-    }
-
-    @Test
-    public void shouldReturnNextMondayOnSaturday() {
-        //given
-        LocalDate today = new LocalDate(2016, 12, 17);
-        LocalDate nextMonday = new LocalDate(2016, 12, 19);
-        //when
-        List<LocalDate> result = TimetableUtils.getNextFullWeekStarts(today);
-        //then
-        Assert.assertEquals(Lists.newArrayList(nextMonday), result);
-    }
-
-    @Test
-    public void shouldReturnNextMondayOnSunday() {
-        //given
-        LocalDate today = new LocalDate(2016, 12, 18);
-        LocalDate nextMonday = new LocalDate(2016, 12, 19);
-        //when
-        List<LocalDate> result = TimetableUtils.getNextFullWeekStarts(today);
-        //then
-        Assert.assertEquals(Lists.newArrayList(nextMonday), result);
-    }
-
-    @Test
-    public void shouldReturnPrevAndNextMondayOnNormalDays() {
+    public void shouldReturnPrevAndNextMonday() {
         //given
         LocalDate today = new LocalDate(2016, 12, 20);
         LocalDate prevMonday = new LocalDate(2016, 12, 19);
