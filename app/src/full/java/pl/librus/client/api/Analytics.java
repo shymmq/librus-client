2a74918154ec06a432f4808e4b2b6b294b24e286
Szymek <szymek.wysocki@gmail.com>
2017-03-21 21:02:23 +0100

    Disabled firebase in debug configuration. Fixes #110

--- app/src/full/java/pl/librus/client/api/Analytics.java
+++ app/src/full/java/pl/librus/client/api/Analytics.java
@@ -2,12 +2,17 @@ package pl.librus.client.api;
 
 import android.app.Application;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+import pl.librus.client.BuildConfig;
+
 /**
  * Created by szyme on 26.02.2017.
  */
 public class Analytics implements IAnalytics {
     @Override
     public void init(Application app) {
-//        Firebase doesn't need initialization
+        FirebaseAnalytics.getInstance(app)
+                .setAnalyticsCollectionEnabled(!BuildConfig.DEBUG);
     }
 }
